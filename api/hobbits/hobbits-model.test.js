ed2a1891a5fd90258b369258f2f1cd1e3252bca2
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-20 18:57:06 +0200

    wip

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -18,11 +18,9 @@ afterAll(async () => {
 describe('Hobbits', () => {
 
   describe('sanity', () => {
-
     test('Hobbit is defined', () => {
       expect(Hobbit).toBeDefined()
     })
-
     test('Environment is correct', () => {
       expect(process.env.DB_ENV).toBe('testing')
     })
