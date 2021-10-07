4c1678c9d025ccfd29e6ab67c0e55d298861b8be
Petr Gazarov <petrgazarov@gmail.com>
2021-10-06 18:55:38 -0700

    Add test script and the hobbit test file

---
+++ api/hobbits/hobbits-model.test.js
@@ -0,0 +1,10 @@
+describe('Hobbit model', () => {
+  // describe for each method
+  // each describe will have 1 or more tests
+
+  describe('getById()', () => {
+    it('retrives hobbit by it', () => {
+      
+    });
+  });
+});
