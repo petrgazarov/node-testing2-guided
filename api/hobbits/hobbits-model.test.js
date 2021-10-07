0c2cb2c2894bbf3c96cda8320b51b3d8716fd77c
Petr Gazarov <petrgazarov@gmail.com>
2021-10-06 19:09:19 -0700

    Add cross-env; fix NODE_ENV issue in test environment

--- api/hobbits/hobbits-model.test.js
+++ api/hobbits/hobbits-model.test.js
@@ -1,10 +1,12 @@
+const Hobbit = require('./hobbits-model');
+
 describe('Hobbit model', () => {
   // describe for each method
   // each describe will have 1 or more tests
 
   describe('getById()', () => {
     it('retrives hobbit by it', () => {
-      
+
     });
   });
 });
--- data/dbConfig.js
+++ data/dbConfig.js
@@ -4,4 +4,6 @@ const config = require("../knexfile.js");
 
 const environment = process.env.NODE_ENV || "development";
 
+console.log('environment', environment);
+
 module.exports = knex(config[environment]);
